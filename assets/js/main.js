97a93fe3e7fa25303fbb10d4593dbc5aa1c7748e
Lucianor01 <lucianorosario02@gmail.com>
2023-08-24 17:08:34 +0200

    elementi nel dom

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+//? ELEMENTI NEL DOM
+const days = document.querySelector('#days');
+const hours = document.querySelector('#hours');
+const minutes = document.querySelector('#minutes');
+const seconds = document.querySelector('#seconds');
+const panel = document.querySelector('.panel');
