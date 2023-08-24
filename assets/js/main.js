bcace75870d61d4b44c05c356ed573372aa46664
Lucianor01 <lucianorosario02@gmail.com>
2023-08-24 17:17:03 +0200

    funzione timer e calcolo la data di natale

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,13 @@ const hours = document.querySelector('#hours');
 const minutes = document.querySelector('#minutes');
 const seconds = document.querySelector('#seconds');
 const panel = document.querySelector('.panel');
+
+function timer(data) {
+    // Creo un oggetto Date per la data di Natale dell'anno corrente
+    const dataNatale = new Date(data.getFullYear(), 11, 25);
+
+    // Calcolo la differenza in millisecondi tra la data corrente e la data di Natale
+    const diff = dataNatale - data
+
+
+}
